ce46325d1405ddc5d4d3aab41a9af04bb85cd1aa
Istvan <17463906+purple0CaT@users.noreply.github.com>
2021-11-02 17:31:40 +0200

    multiple authors

--- src/middlewares/postAuth.js
+++ src/middlewares/postAuth.js
@@ -3,9 +3,11 @@ import PostSchema from "../post/schema.js";
 
 export const checkPost = async (req, res, next) => {
   const post = await PostSchema.findById(req.params.id);
-  if (post.author.toString() === req.user._id.toString()) {
-    next();
-  } else {
-    next(createHttpError(403, "You are not allowed!"));
-  }
+  post.author.map((auth) => {
+    if (auth.toString() === req.user._id.toString()) {
+      next();
+    } else {
+      next(createHttpError(403, "You are not allowed!"));
+    }
+  });
 };
--- src/post/schema.js
+++ src/post/schema.js
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt";
 const { Schema, model } = mongoose;
 
 const PostSchema = new Schema({
-  author: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+  author: [{ type: Schema.Types.ObjectId, required: true, ref: "User" }],
   title: { type: String, required: true },
   text: { type: String, required: true },
 });
